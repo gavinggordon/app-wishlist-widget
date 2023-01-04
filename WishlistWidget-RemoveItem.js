e68963164e6cb593e159bea5b33896b72bf2561c
Gavin Gordon <gavin.gordon@me.com>
2023-01-03 21:35:19 -0800

    Added WishlistWidget-RemoveItem.js

---
+++ WishlistWidget-RemoveItem.js
@@ -0,0 +1,37 @@
+const PARAMS = args.queryParameters
+const APP_ID = PARAMS['id']
+const FM = FileManager.local()
+const LIB_DIR = FM.libraryDirectory()
+const SAVE_DIR_NAME = 'appwishlistwidget'
+const SAVE_DIR_PATH = FM.joinPath(LIB_DIR, SAVE_DIR_NAME)
+const DB_FNAME = 'appwishlist_db.json'
+const DB_FPATH = FM.joinPath(SAVE_DIR_PATH, DB_FNAME)
+
+
+var wishlist
+var wishlistjson
+var removedAppName
+var updatedwishlistjson
+var updatedwishlist = {}
+
+if(FM.fileExists(DB_FPATH)){
+	wishlistjson = await FM.readString(DB_FPATH)
+}
+
+wishlist = JSON.parse(wishlistjson)
+
+for(const [key, value] of Object.entries(wishlist)){
+	if(key != APP_ID){
+		updatedwishlist[key] = value
+	} else {
+		removedAppName = value.name
+	}
+}
+
+updatedwishlistjson = JSON.stringify(updatedwishlist)
+FM.writeString(DB_FPATH, updatedwishlistjson)
+
+let alert = new Alert()
+alert.message = '"' + removedAppName + '" was removed from the App Wishlist.'
+alert.title = 'App Wishlist'
+alert.present()
